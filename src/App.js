1b44f5cc72d927c786cc8b24c688e0344d643fa5
binyoma <innocentbinyoma@pm.me>
2022-09-27 08:30:42 +0200

    toggle button, remove tour

--- src/App.js
+++ src/App.js
@@ -5,33 +5,48 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  const [loading, setLoading]=useState(true);
-  const [tours, setTours]=useState([])
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
 
-  const fetchTours= async () =>{
-    setLoading(true); 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setLoading(true)
     try {
-      const response =await fetch(url);
-      const tours = await response.json();
-      setTours(tours);
+      const response = await fetch(url)
+      const tours = await response.json()
+      setTours(tours)
       setLoading(false)
     } catch (error) {
       setLoading(false)
-      console.log(error);
-      
+      console.log(error)
     }
-   
   }
-  useEffect(()=>{
-    fetchTours();
-  },[])
+  useEffect(() => {
+    fetchTours()
+  }, [])
   if (loading) {
     return
-    <Loading/>
+    ;<Loading />
   }
-  return <main>
-    <Tours tours={tours}/>
-  </main>
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}> refresh</button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
--- src/Tour.js
+++ src/Tour.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Tour = ({id, image, info, price, name}) => {
-  return <article className='single-tour'>
-    <img src={image} alt={name}/>
-    <footer>
-        <div className='tour-info'>
-            <h4>{name}</h4>
-            <h4 className='tour-price'>${price}</h4>
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false)
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
-        <button className='delete-btn'>not interested</button>
-    </footer>
-  </article>;
-};
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => {
+              setReadMore(!readMore)
+            }}
+          >
+            {readMore ? 'show less' : 'show more'}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  )
+}
 
-export default Tour;
+export default Tour
